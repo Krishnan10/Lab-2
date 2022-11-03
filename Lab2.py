0ef46b986556bace97b1009dd8be1dbf16a1fcec
Krishnan <skrishnan.21@ichat.sp.edu.sg>
2022-11-03 16:04:46 +0800

    first draft

---
+++ Lab2.py
@@ -0,0 +1,24 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight / (height * height)
+    print("BMI = " + str(bmi))
+
+    if bmi < 18.5:
+        {
+            print("Under Weight")
+        }
+
+    elif 18.5 <= bmi <= 25.0:
+        {
+            print("Normal Weight")
+        }
+
+    elif bmi > 25:
+        {
+            print("Over Weight")
+        }
+
+
+calculate_bmi(weight=77, height=1.73)
